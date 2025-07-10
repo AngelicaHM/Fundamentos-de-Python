061ccdd38a287750b968ce9ccc84f4b53eccb3d8
AngelicaHM <hdzmo.angelica@gmail.com>
2025-07-10 08:54:07 -0600

    Se agregó operadores lógicos
    Se ejemplificaron las tablas de verdad de NOT, AND y OR.

--- Fundamentos.py
+++ Fundamentos.py
@@ -1,7 +1,7 @@
-if __name__ == "__main__":# esto se utiliza para indicar que línea inicia primero, ya que Python no usa main. No es obligatorio para todo solo cuando es necesario indicar un orden de inicio.
+if __name__ == "__main__":# esto se utiliza para indicar qué línea inicia primero, ya que Python no usa main. No es obligatorio para todo solo cuando es necesario indicar un orden de inicio.
 
     print("-----Operadores aritméticos-----")#Para incluir las líneas dentro del if debemos darle espacio con el tabulador y mantener la misma distancia para las líneas que pertenezcan al Código.
-    print("Operador suma, 7 + 10=",7 + 10)
+    print("Operador suma, 7 + 10 =",7 + 10)
     print("Operador resta, 7 - 10 =",7 - 10)
     print("Operador multiplicación, 7 * 10 =",7 * 10)
     print("Operador división, 7 / 10 =",7 / 10)
@@ -9,11 +9,22 @@ if __name__ == "__main__":# esto se utiliza para indicar que línea inicia prime
     print("Operador módulo, 7 % 10 =",7 % 10)
     print("Operador exponente, 7 ** 10 =",7 ** 10)
     print("Operador identidad, + 7  =",+ 7 )
-    print("Operador cambio de signo-7 =",-7)
+    print("Operador cambio de signo -7 =",-7)
 
     #Este es un comentario de una sola línea
     '''Este es un comentario
     de varias líneas'''
     print("-----Operadores lógicos-----")
-    print("Operador AND")
-    print(True and True)
+    print("  ---Operador NOT---")
+    print("not True: ", not True )
+    print("not False: ", not False )
+    print("  ---Operador AND---")
+    print("True and True:", True and True)
+    print("True and False:", True and False)
+    print("False and True:", False and True)
+    print("False and False:", False and False)
+    print("  ---Operador OR---")
+    print("True or True:", True or True)
+    print("True or False:", True or False)
+    print("False or True:", False or True)
+    print("False or False:", False or False)
