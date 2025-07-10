c716e66c3ea232136b3dc3a9e43ca2c09209c227
AngelicaHM <hdzmo.angelica@gmail.com>
2025-07-10 17:18:29 -0600

    Se agrego un nueva forma de crear funciones
    Utilizando f

--- Fundamentos.py
+++ Fundamentos.py
@@ -1,8 +1,15 @@
-#Crear una función para el comando print, ya que es repetitivo
-def nuevoTema(tema):
-    print("\n----- ", tema, " -----\n")# forma 1 con caracteres de escape
+#Crear una función para el comando print, ya que es repetitivo.
+def nuevoTema(tema:str): #En esta función se indido el tipo de variable con fines informativos, ya que no la está definiendo como tal. Esto se concidera buenas prácticas de programación, nada más.
+    print("\n----- ", tema, " -----\n") # forma 1 con caracteres de escape.
 def nuevoTema2(tema):
-    print("\n----- " + tema + " -----\n")# forma 2 concatenando
+    print("\n----- "  + tema +  " -----\n") # forma 2 concatenando.
+def nuevoTema3(tema):
+    print(f"\n-----   {tema}  -----\n") # forma 3 con formato (con más estilo Python)también llamada "cadena formateada".
+def nuevoTema4(tema):
+    print(f"\n-----   {tema}  -----\n")
+    llave_a = "{" #Se pueden crear fvariables para signos que podrian confuncdirse con los comandos del lenguaje. 
+    llave_c = "}" #Por ejemplo llaves de apertura y de cierre
+    print(f"\n----- {llave_a} {tema} {llave_c} -----\n")
 
 if __name__ == "__main__":# esto se utiliza para indicar qué línea inicia primero, ya que Python no usa main. No es obligatorio para todo solo cuando es necesario indicar un orden de inicio.
 
@@ -21,15 +28,15 @@ if __name__ == "__main__":# esto se utiliza para indicar qué línea inicia prim
     '''Este es un comentario
     de varias líneas'''
     print("-----Operadores lógicos-----")
-    print("  ---Operador NOT---")
+    print("  ---Operador NOT---") # Comprobación de tabla de verdad de NOT
     print("not True: ", not True )
     print("not False: ", not False )
-    print("  ---Operador AND---")
+    print("  ---Operador AND---") # Comprobación de tabla de verdad de AND
     print("True and True:", True and True)
     print("True and False:", True and False)
     print("False and True:", False and True)
     print("False and False:", False and False)
-    print("  ---Operador OR---")
+    print("  ---Operador OR---") # Comprobación de tabla de verdad de OR
     print("True or True:", True or True)
     print("True or False:", True or False)
     print("False or True:", False or True)
@@ -46,3 +53,5 @@ if __name__ == "__main__":# esto se utiliza para indicar qué línea inicia prim
 #Crear Funciones
 nuevoTema("Operadores aritméticos") #usando caracteres de escape
 nuevoTema2("Operadores aritméticos") # usando concatenación
+nuevoTema3("Operadores aritméticos") # usando una "cadena formateada"
+nuevoTema4("Operadores aritméticos") # incluyendo otras variables.
