378d2bf6d561b21eb7da52d561d0a373687e808a
AngelicaHM <hdzmo.angelica@gmail.com>
2025-07-10 08:40:06 -0600

    Se agregan operadores aritméticos
    Ejemplo de cada operador aritmético

---
+++ Fundamentos.py
@@ -0,0 +1,19 @@
+if __name__ == "__main__":# esto se utiliza para indicar que línea inicia primero, ya que Python no usa main. No es obligatorio para todo solo cuando es necesario indicar un orden de inicio.
+
+    print("-----Operadores aritméticos-----")#Para incluir las líneas dentro del if debemos darle espacio con el tabulador y mantener la misma distancia para las líneas que pertenezcan al Código.
+    print("Operador suma, 7 + 10=",7 + 10)
+    print("Operador resta, 7 - 10 =",7 - 10)
+    print("Operador multiplicación, 7 * 10 =",7 * 10)
+    print("Operador división, 7 / 10 =",7 / 10)
+    print("Operador división entera, 7 // 10 =",7 // 10)
+    print("Operador módulo, 7 % 10 =",7 % 10)
+    print("Operador exponente, 7 ** 10 =",7 ** 10)
+    print("Operador identidad, + 7  =",+ 7 )
+    print("Operador cambio de signo-7 =",-7)
+
+    #Este es un comentario de una sola línea
+    '''Este es un comentario
+    de varias líneas'''
+    print("-----Operadores lógicos-----")
+    print("Operador AND")
+    print(True and True)
