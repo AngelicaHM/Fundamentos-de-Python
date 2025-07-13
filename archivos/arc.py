bb1c632e5c2b0ebb1be84f1c10d22479a8b01a88
AngelicaHM <hdzmo.angelica@gmail.com>
2025-07-13 03:40:52 -0600

    Uso de With
    El with se usa para abrir y usar un recurso temporalmente, y cerrarlo automáticamente al terminar.

--- archivos/arc.py
+++ archivos/arc.py
@@ -3,9 +3,35 @@ file.write("Este es el contenido del archivo.")
 file.close()
 
 lista= ["Fresa", "Mango", "Papaya"]  
-con 
+
 with open("EjemploArchivo.txt", "a") as file:    #La instrucción with junto con un recurso, se crea un contexto de uso o de aplicación del recurso, cierra el archivo al finalizar las instrucciones.
     for e in lista:
         file.write(e + "\n")                      #Añade la informaicón de lista al archivo existente.
 
 print("Lista escrita en el archivo")
+
+lista2 = ["Honda", "Suzuki", "BMW"]
+
+with open("EjemploArchivo.txt", "a") as file:
+    file.writelines(lista2)
+
+print("Lista escrita con writelines")
+
+print("Imprimir todo el contenido del archivo")
+
+with open("EjemploArchivo.txt", "r") as file:
+    print(file.read())
+
+print("Leer dos líneas y posteriormente 5 caracteres")
+with open("EjemploArchivo.txt", "r") as file:
+    print(file.readline())
+    print(file.readline())
+    print(file.readline(5))
+
+print("Almacenar el contenido de una lista y mostrar el último elemento")
+with open("EjemploArchivo.txt", "r") as file:
+    contenido = file.readlines()
+    print (contenido[-1])
+
+
+
