dcefbf64781cb54d1277f28dd356ddddf2d879cf
AngelicaHM <hdzmo.angelica@gmail.com>
2025-07-12 23:42:02 -0600

    Definición de operaciones aritméticas básicas
    Se definen las funciones de operaciones aritméticas básicas, como suma, resta, multiplicación y división para una calculadora.

---
+++ Calc/operaciones.py
@@ -0,0 +1,25 @@
+def resta(*nums:float)+>float:         #Definimos función de resta
+    res = 0
+    for valor in nums:
+        res -= valor
+    return res
+def suma(*nums:float)->float:          #Definimos función de suma
+    res= 0
+    for valor in nums:
+        res += valor
+    return res
+
+def mult(*nums:float)->float:          #Definimos función de multiplicación
+    res= 1
+    for valor in nums:
+        res *= valor
+    return res
+
+def div(*n1:float, n2:float)->float:          #Definimos función de división
+    res = n1/n2
+    sobra = n1 % n2
+    
+    return res, sobra
+
+
+
