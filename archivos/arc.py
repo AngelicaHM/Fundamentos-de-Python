2b74f273cc63c2ff5d296e2a3f3651a4de98fd8e
AngelicaHM <hdzmo.angelica@gmail.com>
2025-07-13 01:35:55 -0600

    Se agregó bloque with
    El bloque with abre y utiliza recursos de forma temporal y despúes los cierra.

--- archivos/arc.py
+++ archivos/arc.py
@@ -1,6 +1,11 @@
-file = open("EjemploAchivo.txt", "w")
+file = open("EjemploArchivo.txt", "w")
 file.write("Este es el contenido del archivo.")
 file.close()
 
-lista= ["Fresa", "Mango", "Papaya"]
+lista= ["Fresa", "Mango", "Papaya"]  
+con 
+with open("EjemploArchivo.txt", "a") as file:    #La instrucción with junto con un recurso, se crea un contexto de uso o de aplicación del recurso, cierra el archivo al finalizar las instrucciones.
+    for e in lista:
+        file.write(e + "\n")                      #Añade la informaicón de lista al archivo existente.
 
+print("Lista escrita en el archivo")
